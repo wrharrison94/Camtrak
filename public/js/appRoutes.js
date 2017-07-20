2709821bc4826816f9fbdc08c3987752000bad07
“Will <wrharrison94@gmail.com>
2017-07-20 17:56:28 -0500

    Ready For Multiple Controllers in index.html
    lenses page no longer routes as lenses.html

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,7 +8,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'MainController'
 		})
 
-		.when('/lenses.html', {
+		.when('/lenses', {
 			templateUrl: 'views/lenses.html',
 			controller: 'LensesController'
 		})
